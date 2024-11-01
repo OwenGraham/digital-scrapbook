cb741aa3214b4c2d2b8d33839174d2f847707577
OwenGraham <71776173+OwenGraham@users.noreply.github.com>
2024-11-01 15:59:45 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ import App from './App.jsx';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AppWrapper />
+    <App />
   </React.StrictMode>
 );
 
