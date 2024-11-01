38842cd14d80327a91d4c36d72ca72761118b17e
Owen Graham <owen.graham@outlook.com>
2024-11-01 16:14:10 +0000

    hopefully fixed eslint warnings, maybe then build will work

--- src/components/scraps/Recipe.jsx
+++ src/components/scraps/Recipe.jsx
@@ -1,7 +1,7 @@
 export default function Recipe({ img, title, cookingTime, recipe }) {
   return (
     <section className="scrap">
-      <img src={img} alt="Image of food" />
+      <img src={img} alt="Finished recipe" />
       <h2>{title}</h2>
       <p>Cooking time = {cookingTime} mins</p>
       <div>
