a88b60eb408568a94077152a6044752dc1048e50
Owen Graham <owen.graham@outlook.com>
2024-11-28 11:20:52 +0000

    removed isbn from books. Changed event lineups and recipes to use lists

--- src/components/scraps/Recipe.jsx
+++ src/components/scraps/Recipe.jsx
@@ -1,11 +1,22 @@
-export default function Recipe({ img, name, cookingTime, recipe }) {
+export default function Recipe({ img, name, cookingTime, ingredients, recipe }) {
   return (
     <section className="scrap">
       <img src={img} alt="Finished recipe" />
       <h2>{name}</h2>
       <p>Cooking time = {cookingTime} mins</p>
       <div>
-        <p>{recipe}</p>
+        <h3>Ingredients</h3>
+        <ul>
+          {ingredients.map((ingredient, index) => (
+            <li key={index}>{ingredient}</li>
+          ))}
+        </ul>
+        <h3>Recipe</h3>
+        <ul>
+          {recipe.map((step, index) => (
+            <li key={index}>{step}</li>
+          ))}
+        </ul>
       </div>
     </section>
   );
