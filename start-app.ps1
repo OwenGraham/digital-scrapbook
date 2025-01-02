82f4c09c986045f88171f1bc7afff20614b16a94
Owen Graham <owen.graham@outlook.com>
2025-01-02 18:09:47 +0000

    updated script to be more portable

--- start-app.ps1
+++ start-app.ps1
@@ -1,5 +1,11 @@
+# Ensure npm is in the PATH
+$env:PATH += ";C:\Program Files\nodejs"
+
+# Get the script directory
+$scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
 # Start the backend server
-Start-Process -NoNewWindow -FilePath "powershell" -ArgumentList "cd backend; mvn spring-boot:run"
+Start-Process -NoNewWindow -FilePath "powershell" -ArgumentList "cd `"$scriptDir\backend`"; mvn spring-boot:run"
 
 # Start the frontend server
-Start-Process -NoNewWindow -FilePath "powershell" -ArgumentList "cd frontend; npm start"
+Start-Process -NoNewWindow -FilePath "powershell" -ArgumentList "cd `"$scriptDir\frontend`"; npm start"
